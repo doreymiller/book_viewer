d587b4ebdf60d435252c3f1eba1211a46678a5ab
Dorey Miller <doreymiller@Doreys-MacBook-Pro.local>
2020-01-16 11:56:20 -0800

    commiting book viewer for first push

---
+++ book_viewer.rb
@@ -0,0 +1,76 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map.with_index { |par, index| "<p id=#{index}>#{par}</p>" }.join
+  end
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  text = File.read("data/chp#{number}.txt")
+
+  @title = "Chapter #{number}"
+  @chapter = in_paragraphs(text)
+  @chapter_title = "Chapter #{number}: #{@contents[number.to_i - 1]}"
+
+  erb :chapter
+end
+
+get "/show/:name" do
+  params[:name]
+end
+
+get "/search" do
+  @results = []
+
+  return @results if params[:query].nil?
+  
+  search_chapters
+
+  erb :search
+end
+
+def search_chapters
+  each_chapter do |title, number, text|
+    chapter_results = {title: title, chapter_num: number, text: [], ids: []}
+      
+    text.split("\n\n").each_with_index do |paragraph, index|
+       matches = paragraph.match(params[:query])
+
+      if matches
+        chapter_results[:text] << paragraph.gsub(params[:query], "<strong>#{params[:query]}</strong>")
+        chapter_results[:ids] << index
+      end
+    end
+
+    @results << chapter_results if chapter_results[:text].any?
+  end
+end
+
+def each_chapter
+  @contents.each_with_index do |title, index|
+    number = index + 1
+    text = File.read("data/chp#{number}.txt")
+    yield(title, number, text)
+  end
+end
+
+not_found do
+  redirect "/"
+end
